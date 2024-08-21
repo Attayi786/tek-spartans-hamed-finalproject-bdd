170b3fd9f229d7ecf91572d6c466be4016b4b359
mydocuments <hamed.mr.unique@gmail.com>
2024-08-20 23:48:13 -0700

    creating final project

---
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -0,0 +1,15 @@
+package tek.bdd.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "tek.bdd.steps",
+        tags = "@Scenario_2",
+        dryRun = false
+)
+public class TestRunner {
+}
