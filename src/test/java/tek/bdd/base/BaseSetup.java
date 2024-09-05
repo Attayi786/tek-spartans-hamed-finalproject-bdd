8fd07392ed739ec7577643d2138d023d9c8d5782
mydocuments <hamed.mr.unique@gmail.com>
2024-09-04 20:24:11 -0700

    final project bdd submit stage Updated

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -9,7 +9,6 @@ import tek.bdd.browsers.EdgeBrowser;
 import tek.bdd.browsers.FireFoxBrowser;
 import tek.bdd.exceptions.ExceptionsHandling;
 import tek.bdd.utilities.Constants;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,35 +16,40 @@ import java.time.Duration;
 import java.util.Properties;
 
 /**
- * This abstract class sets up and manages a WebDriver instance for browser interactions.
+ * BaseSetup is an abstract class that provides common setup and tear down functionalities
+ * for initializing and managing WebDriver instances in test automation.
  */
 public abstract class BaseSetup {
 
-    // Static WebDriver instance shared across all subclasses
+    // Static WebDriver instance shared across subclasses
     private static WebDriver driver;
 
-    // Logger instance for logging various actions and events
+    // Logger for logging messages and debugging information
     private static final Logger LOGGER = LogManager.getLogger(BaseSetup.class);
 
-    // Properties instance to load and manage configuration settings
+    // Properties object to hold configuration settings from a properties file
     private final Properties properties;
 
     /**
-     * Constructor that initializes the Properties object by loading the configuration file.
+     * Constructor for BaseSetup class. It loads the configuration from a properties file.
      */
     public BaseSetup() {
         try {
-            // Path to the configuration file
+            // Construct the path to the configuration file
             String configFilePath = System.getProperty("user.dir") + "/src/test/resources/configs/dev-config.properties";
             LOGGER.info("Reading Config file {}", configFilePath);
 
-            // Load the configuration file into the Properties object
+            // Create a File object for the configuration file
             File file = new File(configFilePath);
+
+            // Create a FileInputStream to read the configuration file
             FileInputStream fileInputStream = new FileInputStream(file);
+
+            // Initialize Properties object and load properties from the file
             properties = new Properties();
             properties.load(fileInputStream);
         } catch (IOException exception) {
-            // Log error and throw custom exception if there's an issue reading the file
+            // Log error and throw a custom exception if there is an issue reading the config file
             LOGGER.error("Error reading config file", exception);
             throw new ExceptionsHandling("Something wrong with config file");
         }
@@ -55,25 +59,30 @@ public abstract class BaseSetup {
      * Opens the browser based on the configuration settings and navigates to the specified URL.
      */
     public void openBrowser() {
-        // Retrieve browser type and headless mode from properties
+        // Retrieve browser type and headless mode settings from the properties file
         String browserType = properties.getProperty("ui.browser");
         boolean isHeadless = Boolean.parseBoolean(properties.getProperty("ui.browser.headless"));
+
         LOGGER.info("Running on browser {}", browserType);
         LOGGER.info("Is Headless ON {}", isHeadless);
-
-        // Create the appropriate browser instance based on the configuration
         BaseBrowser browser;
-        if (browserType.equalsIgnoreCase("chrome"))
-            browser = new ChromeBrowser();
-        else if (browserType.equalsIgnoreCase("edge"))
-            browser = new EdgeBrowser();
-        else if (browserType.equalsIgnoreCase("firefox"))
-            browser = new FireFoxBrowser();
-        else
-            throw new ExceptionsHandling("Wrong browser type, choose another browser such as chrome, firefox and edge");
 
-        // Open the browser and navigate to the URL
+        // Instantiate the appropriate browser based on the configuration
+        if (browserType.equalsIgnoreCase("chrome")) {
+            browser = new ChromeBrowser();
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            browser = new EdgeBrowser();
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            browser = new FireFoxBrowser();
+        } else {
+            // Throw a custom exception if the browser type is unsupported
+            throw new ExceptionsHandling("Wrong browser type, choose another browser such as chrome, firefox and edge");
+        }
+
+        // Open the browser and set the WebDriver instance
         driver = browser.openBrowser(isHeadless);
+
+        // Retrieve the URL from the properties file and navigate to it
         String url = properties.getProperty("ui.url");
         LOGGER.debug("Using URL {}", url);
         driver.get(url);
@@ -84,8 +93,9 @@ public abstract class BaseSetup {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Constants.WAIT_IN_SECONDS));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Constants.WAIT_IN_SECONDS));
     }
+
     /**
-     * Closes the browser and cleans up the WebDriver instance.
+     * Quits the browser and closes all associated windows.
      */
     public void quitBrowser() {
         if (driver != null) {
@@ -93,9 +103,11 @@ public abstract class BaseSetup {
             driver.quit();
         }
     }
+
     /**
-     * Returns the current WebDriver instance.
-     * @return WebDriver instance
+     * Gets the WebDriver instance.
+     *
+     * @return The WebDriver instance.
      */
     public WebDriver getDriver() {
         LOGGER.info("Getting the driver");
--- src/test/java/tek/bdd/steps/CreateAccountSteps.java
+++ src/test/java/tek/bdd/steps/CreateAccountSteps.java
@@ -1,90 +1,90 @@
 package tek.bdd.steps;
 
-import io.cucumber.java.en.Then; // Importing Cucumber annotation for 'Then' steps
-import io.cucumber.java.en.When; // Importing Cucumber annotation for 'When' steps
-import org.junit.Assert; // Importing JUnit assertion methods
-import tek.bdd.pages.CreateAccountPageObject; // Importing page object class for Create Account page
-import tek.bdd.utilities.RandomGenerator; // Importing utility class for generating random data
-import tek.bdd.utilities.SeleniumUtility; // Importing utility class for common Selenium WebDriver methods
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import tek.bdd.pages.CreateAccountPageObject;
+import tek.bdd.utilities.RandomGenerator;
+import tek.bdd.utilities.SeleniumUtility;
 
 public class CreateAccountSteps extends SeleniumUtility {
 
-    @When("click on Crate Primary Account link") // Cucumber step definition for clicking the 'Create Primary Account' link
+    @When("click on Crate Primary Account link")
     public void clickOnLink() {
-        clickOnElement(CreateAccountPageObject.CREATE_PRIMARY_ACCOUNT_LINK); // Click on the specified link element
+        clickOnElement(CreateAccountPageObject.CREATE_PRIMARY_ACCOUNT_LINK);
     }
 
-    @Then("validate section title to be Create Primary Account Holder") // Cucumber step definition for validating the section title
+    @Then("validate section title to be Create Primary Account Holder")
     public void validateSectionTitleToBe() {
-        String createAccountHolderTitle = getElementText(CreateAccountPageObject.CREATE_PRIMARY_ACCOUNT_HOLDER_TILE); // Get the text of the section title element
-        Assert.assertEquals("Create Primary Account Holder", createAccountHolderTitle); // Assert that the section title is as expected
+        String createAccountHolderTitle = getElementText(CreateAccountPageObject.CREATE_PRIMARY_ACCOUNT_HOLDER_TILE);
+        Assert.assertEquals("Create Primary Account Holder", createAccountHolderTitle);
     }
 
-    @Then("fill up create account form") // Cucumber step definition for filling out the create account form
+    @Then("fill up create account form")
     public void fillUpCreateAccountForm() {
-        String prefix = "Hamed"; // Prefix for generating a random email
-        String randomEmail = RandomGenerator.randomEmail(prefix); // Generate a random email address using the prefix
-        sendText(CreateAccountPageObject.EMAIL_INPUT, randomEmail); // Enter the generated email address into the email input field
-        selectFromDropDown(CreateAccountPageObject.PREFIX_SELECT, "Mr."); // Select "Mr." from the prefix dropdown
-        sendText(CreateAccountPageObject.FIRST_NAME_INPUT, "Hamed"); // Enter first name
-        sendText(CreateAccountPageObject.LAST_NAME_INPUT, "A"); // Enter last name
-        selectFromDropDown(CreateAccountPageObject.GENDER_SELECT, "Male"); // Select "Male" from the gender dropdown
-        selectFromDropDown(CreateAccountPageObject.MARITAL_STATUS_SELECT, "Single"); // Select "Married" from the marital status dropdown
-        sendText(CreateAccountPageObject.EMPLOYMENT_STATUS, "Test Engineer"); // Enter employment status
-        sendText(CreateAccountPageObject.DATE_OF_BIRTH, "07/19/1992"); // Enter date of birth
+        String prefix = "Hamed";
+        String randomEmail = RandomGenerator.randomEmail(prefix);
+        sendText(CreateAccountPageObject.EMAIL_INPUT, randomEmail);
+        selectFromDropDown(CreateAccountPageObject.PREFIX_SELECT, "Mr.");
+        sendText(CreateAccountPageObject.FIRST_NAME_INPUT, "Hamed");
+        sendText(CreateAccountPageObject.LAST_NAME_INPUT, "A");
+        selectFromDropDown(CreateAccountPageObject.GENDER_SELECT, "Male");
+        selectFromDropDown(CreateAccountPageObject.MARITAL_STATUS_SELECT, "Single");
+        sendText(CreateAccountPageObject.EMPLOYMENT_STATUS, "Test Engineer");
+        sendText(CreateAccountPageObject.DATE_OF_BIRTH, "07/19/1992");
     }
 
-    @Then("click on Create Account button") // Cucumber step definition for clicking the 'Create Account' button
+    @Then("click on Create Account button")
     public void clickOnCreateAccountButton() {
-        clickOnElement(CreateAccountPageObject.CREATE_ACCOUNT_BUTTON); // Click on the specified button element
+        clickOnElement(CreateAccountPageObject.CREATE_ACCOUNT_BUTTON);
     }
 
-    @Then("validate Sign up your account title is exist") // Cucumber step definition for validating the "Sign up your account" title
+    @Then("validate Sign up your account title is exist")
     public void validateSignUpYourAccountTitleIsExist() {
-        String actualSignUpTitle = getElementText(CreateAccountPageObject.SIGN_UP_YOUR_ACCOUNT_TITLE); // Get the text of the sign-up title element
-        Assert.assertEquals("Sign up your account", actualSignUpTitle); // Assert that the sign-up title matches the expected value
-    }
+        String actualSignUpTitle = getElementText(CreateAccountPageObject.SIGN_UP_YOUR_ACCOUNT_TITLE);
+        Assert.assertEquals("Sign up your account", actualSignUpTitle);
 
-    @Then("validate user full name is displayed") // Cucumber step definition for checking if the user's full name is displayed
+    }
+    @Then("validate user full name is displayed")
     public void validateUserFullNameIsDisplayed() {
-        boolean userActualFullName = isElementIsDisplayed(CreateAccountPageObject.USER_FULL_NAME); // Check if the user's full name element is displayed
-        Assert.assertTrue("User full name should be displayed", userActualFullName); // Assert that the user's full name is displayed
-    }
+        boolean userActualFullName = isElementIsDisplayed(CreateAccountPageObject.USER_FULL_NAME);
+        Assert.assertTrue("User full name should be displayed", userActualFullName);
 
-    @Then("validate email address shows as expected") // Cucumber step definition for checking if the email address is displayed as expected
+    }
+    @Then("validate email address shows as expected")
     public void validateEmailAddressShowsAsExpected() {
-        boolean userActualEmailAddress = isElementIsDisplayed(CreateAccountPageObject.USER_EMAIL_ADDRESS_TITLE); // Check if the email address element is displayed
-        Assert.assertTrue("Email address must be displayed as expected", userActualEmailAddress); // Assert that the email address is displayed as expected
-    }
+        boolean userActualEmailAddress = isElementIsDisplayed(CreateAccountPageObject.USER_EMAIL_ADDRESS_TITLE);
+        Assert.assertTrue("Email address must be displayed as expected", userActualEmailAddress);
 
-    @When("when the user fill up sign up form") // Cucumber step definition for filling out the sign-up form
+    }
+    @When("when the user fill up sign up form")
     public void whenTheUserFillUpSignUpForm() {
-        sendText(CreateAccountPageObject.USERNAME_INPUT, "Hamed"); // Enter username
-        sendText(CreateAccountPageObject.PASSWORD_INPUT, "Qwerty@1992"); // Enter password
-        sendText(CreateAccountPageObject.CONFIRM_PASSWORD_INPUT, "Qwerty@1992"); // Enter password confirmation
+        sendText(CreateAccountPageObject.USERNAME_INPUT, "Hamed");
+        sendText(CreateAccountPageObject.PASSWORD_INPUT, "Qwerty@1992");
+        sendText(CreateAccountPageObject.CONFIRM_PASSWORD_INPUT, "Qwerty@1992");
     }
-
-    @Then("click on submit button") // Cucumber step definition for clicking the 'Submit' button
+    @Then("click on submit button")
     public void clickOnSubmitButton() {
-        clickOnElement(CreateAccountPageObject.SUBMIT_BUTTON); // Click on the specified submit button element
+        clickOnElement(CreateAccountPageObject.SUBMIT_BUTTON);
     }
 
-    @Then("fill up create account form page") // Cucumber step definition for filling out the create account form on a page
+    @Then("fill up create account form page")
     public void fillUpCreateAccountFormPage() {
-        sendText(CreateAccountPageObject.EMAIL_INPUT, "Hamed.Attayi@tekschool.us"); // Enter a specific email address into the email input field
-        selectFromDropDown(CreateAccountPageObject.PREFIX_SELECT, "Mr."); // Select "Mr." from the prefix dropdown
-        sendText(CreateAccountPageObject.FIRST_NAME_INPUT, "Hamed"); // Enter first name
-        sendText(CreateAccountPageObject.LAST_NAME_INPUT, "A"); // Enter last name
-        selectFromDropDown(CreateAccountPageObject.GENDER_SELECT, "Male"); // Select "Male" from the gender dropdown
-        selectFromDropDown(CreateAccountPageObject.MARITAL_STATUS_SELECT, "Single"); // Select "Married" from the marital status dropdown
-        sendText(CreateAccountPageObject.EMPLOYMENT_STATUS, "Test Engineer"); // Enter employment status
-        sendText(CreateAccountPageObject.DATE_OF_BIRTH, "07/19/1992"); // Enter date of birth
-    }
 
-    @When("validate account error message") // Cucumber step definition for validating the account error message
+        sendText(CreateAccountPageObject.EMAIL_INPUT, "Hamed.Attayi@tekschool.us");
+        selectFromDropDown(CreateAccountPageObject.PREFIX_SELECT, "Mr.");
+        sendText(CreateAccountPageObject.FIRST_NAME_INPUT, "Hamed");
+        sendText(CreateAccountPageObject.LAST_NAME_INPUT, "A");
+        selectFromDropDown(CreateAccountPageObject.GENDER_SELECT, "Male");
+        selectFromDropDown(CreateAccountPageObject.MARITAL_STATUS_SELECT, "Single");
+        sendText(CreateAccountPageObject.EMPLOYMENT_STATUS, "Test Engineer");
+        sendText(CreateAccountPageObject.DATE_OF_BIRTH, "07/19/1992");
+    }
+    @When("validate account error message")
     public void validateAccountErrorMessage() {
-        String errorMessage = getElementText(CreateAccountPageObject.ERROR_MESSAGE); // Get the text of the error message element
-        Assert.assertEquals("ERROR", errorMessage); // Assert that the error message matches the expected value
-        System.out.println(errorMessage); // Print the error message to the console for debugging
+        String errorMessage = getElementText(CreateAccountPageObject.ERROR_MESSAGE);
+        Assert.assertEquals("ERROR", errorMessage);
+        System.out.println(errorMessage);
+
     }
 }
